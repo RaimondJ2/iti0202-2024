52577abf3cf174d057f0ffab743bae61f6f4e63b
TimeCold <timecold@enigmaps.eu>
2025-02-16 20:06:42 +0200

    asdads

--- LX/LX01BigDecimal/src/ee/taltech/iti0202/BigNumber.java
+++ LX/LX01BigDecimal/src/ee/taltech/iti0202/BigNumber.java
@@ -13,7 +13,7 @@ public class BigNumber {
      * @return factor1 x factor2
      */
     public BigInteger multiplyBigInteger(int factor1, int factor2) {
-        int cad = 0;
+        int cadd = 0;
         return BigInteger.valueOf(factor1).multiply(BigInteger.valueOf(factor2));
     }
 
